5f8e5f16dc6dcdd745d72b7468bb08b0a8061251
signapoop <leowyaoyang@gmail.com>
2018-04-09 22:33:21 +0100

    checkpoint

--- experiments/cifar10_singlerun.py
+++ experiments/cifar10_singlerun.py
@@ -7,6 +7,6 @@ data_dir = '/Users/signapoop/Desktop/autotune/data'
 problem = CIFAR10_problem(dirname, data_dir)
 problem.print_domain()
 
-x = np.array([ 81., -3.41513064, 2.04309237, -3.03636196, 0.48027257])
+x = np.array([81., -3.41513064, 2.04309237, -3.03636196, 0.48027257])
 x = np.reshape(x, (1,-1))
 problem.f(x)
