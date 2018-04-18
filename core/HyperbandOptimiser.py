a1f2d9d1dd52750d9f20e382706771730aa76bd5
signapoop <leowyaoyang@gmail.com>
2018-04-18 20:40:11 +0200

    small test

--- core/HyperbandOptimiser.py
+++ core/HyperbandOptimiser.py
@@ -4,11 +4,11 @@ import numpy as np
 from RandomOptimiser import RandomOptimiser
 
 class HyperbandOptimiser(RandomOptimiser):
-    def __init__(self, arms_init=[], Y_init=[]):
-        super(HyperbandOptimiser, self).__init__(arms_init, Y_init)
-        # problem provides generate_random_arm and eval_arm(x)
+    def __init__(self, arms_init=[], val_loss_init=[], Y_init=[]):
+        super(HyperbandOptimiser, self).__init__(arms_init, val_loss_init, Y_init)
 
     def run_optimization(self, problem, n_units=None, max_iter=None, eta=3, verbosity=False):
+        # problem provides generate_random_arm and eval_arm(x)
 
         print("---- Running hyperband optimisation ----")
 
