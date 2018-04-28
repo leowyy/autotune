01f62a57f9b3fbb5d456e34b25e74e81559eb684
signapoop <leowyaoyang@gmail.com>
2018-04-28 12:08:36 +0100

    random run

---
+++ experiments/random_run.py
@@ -0,0 +1,31 @@
+import pickle
+import argparse
+
+from ..core.RandomOptimiser import RandomOptimiser
+# from ..benchmarks.mnist_problem import MnistProblem
+# from ..benchmarks.cifar_problem import CifarProblem
+from ..benchmarks.svhn_problem import SvhnProblem
+
+parser = argparse.ArgumentParser(description='PyTorch Training')
+parser.add_argument('-i', '--input_dir', type=str, help='input dir')
+parser.add_argument('-o', '--output_dir', type=str, help='output dir')
+parser.add_argument('-res', '--n_resources', default=3, type=int, help='n_resources')
+args = parser.parse_args()
+
+print("Input directory: {}".format(args.input_dir))
+print("Output directory: {}".format(args.output_dir))
+print("# resources: {}".format(args.n_resources))
+
+# Define problem instance
+problem = SvhnProblem(args.input_dir, args.output_dir)
+problem.print_domain()
+
+# Define maximum units of resource assigned to each optimisation iteration
+n_resources = args.n_resources
+
+random_opt = RandomOptimiser()
+random_opt.run_optimization(problem, n_resources, max_iter=64, verbosity=True)
+
+filename = args.output_dir + 'results.pkl'
+with open(filename, 'wb') as f:
+    pickle.dump([random_opt], f)
