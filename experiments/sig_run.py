5a3175b26e2bc8c9221a896cf3b93ff64933de4d
signapoop <leowyaoyang@gmail.com>
2018-07-15 18:51:01 +0100

    SigOpt and branin

---
+++ experiments/sig_run.py
@@ -0,0 +1,32 @@
+import pickle
+import argparse
+
+from ..core.SigOptimiser import SigOptimiser
+from ..benchmarks.cifar_problem import CifarProblem
+
+parser = argparse.ArgumentParser(description='PyTorch Training')
+parser.add_argument('-i', '--input_dir', type=str, help='input dir')
+parser.add_argument('-o', '--output_dir', type=str, help='output dir')
+parser.add_argument('-res', '--n_resources', default=3, type=int, help='n_resources')
+args = parser.parse_args()
+
+print("Input directory: {}".format(args.input_dir))
+print("Output directory: {}".format(args.output_dir))
+print("# resources: {}".format(args.n_resources))
+
+# Define problem instance
+problem = CifarProblem(args.input_dir, args.output_dir)
+problem.print_domain()
+
+# Define maximum units of resource assigned to each optimisation iteration
+n_resources = args.n_resources
+
+sig_opt = SigOptimiser()
+sig_opt.run_optimization(problem, n_resources, max_iter=5, verbosity=True)
+
+filename = args.output_dir + 'results.pkl'
+with open(filename, 'wb') as f:
+    pickle.dump([sig_opt], f)
+
+print(sig_opt.arm_opt)
+print(sig_opt.fx_opt)
